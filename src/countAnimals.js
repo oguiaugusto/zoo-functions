930dfcb8888eb8906efd4516c0108c4c22dfbe38
Guilherme <guilhaugusto05@gmail.com>
2021-10-11 23:09:55 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const allAnimals = () =>
+  species.reduce((acc, specie, index) => {
+    const { name, residents } = specie;
+    if (index === 0) return { [name]: residents.length };
+    return { ...acc, [name]: residents.length };
+  }, '');
+
+function countAnimals(animal = 'all') {
+  if (animal === 'all') return allAnimals();
+  const { specie, sex = 'both' } = animal;
+  const specieInfo = species.find((anim) => anim.name === specie);
+  const specieResidents = specieInfo.residents;
+  const amount = specieResidents.reduce((acc, resident) => {
+    if (sex === 'both') return acc + 1;
+    return (resident.sex === sex) ? acc + 1 : acc;
+  }, 0);
+  return amount;
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
