d7d2049dc13b8663223ebfb8f8f6ec203dc71173
Guilherme <guilhaugusto05@gmail.com>
2021-10-12 00:40:34 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  return { adult: adult.length, senior: senior.length, child: child.length };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = 'none') {
+  if (entrants === 'none' || Object.keys(entrants).length < 1) return 0;
+  const entries = Object.values(countEntrants(entrants));
+  const prices = Object.values(data.prices);
+  return prices.reduce((acc, price, index) => {
+    const person = entries[index];
+    return acc + price * person;
+  }, 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
