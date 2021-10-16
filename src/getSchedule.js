ca0002f65d7c6f94ef9f8a7cbd86b992be5d78f1
Guilherme <guilhaugusto05@gmail.com>
2021-10-16 00:35:24 -0300

    Diminui função getSchdule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -5,28 +5,11 @@ const { hours, species } = data;
 const getExhbition = (day) => species.filter((specie) => specie.availability.includes(day))
   .map((specie) => specie.name);
 
-const getWeekDay = (day) => {
-  if (day !== 'Monday') {
-    const { open, close } = hours[day];
-    return {
-      [day]: {
-        officeHour: `Open from ${open}am until ${close}pm`,
-        exhibition: getExhbition(day),
-      },
-    };
-  }
-  return {
-    [day]: {
-      officeHour: 'CLOSED',
-      exhibition: 'The zoo will be closed!',
-    },
-  };
-};
 const getAnimalDays = (animal) => {
   const searchAnimal = species.find((specie) => specie.name === animal);
   return searchAnimal.availability;
 };
-const getFullShedule = (allDays) => {
+const getDaySchedule = (...allDays) => {
   const schedule = {};
   allDays.forEach((day) => {
     if (day !== 'Monday') {
@@ -49,8 +32,8 @@ function getSchedule(scheduleTarget = 'none') {
   const animals = species.map((specie) => specie.name);
   const weekDays = Object.keys(hours);
   if (animals.includes(scheduleTarget)) return getAnimalDays(scheduleTarget);
-  if (weekDays.includes(scheduleTarget)) return getWeekDay(scheduleTarget);
-  return getFullShedule(weekDays);
+  if (weekDays.includes(scheduleTarget)) return getDaySchedule(scheduleTarget);
+  return getDaySchedule(...weekDays);
 }
 
 module.exports = getSchedule;
