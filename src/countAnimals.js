3a6db04970479a1e15166b1da9ca56845ec4776c
Guilherme <guilhaugusto05@gmail.com>
2022-04-23 16:14:12 -0300

    refractor countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,19 +1,16 @@
 const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
 const allAnimals = () =>
-  species.reduce((acc, specie, index) => {
-    const { name, residents } = specie;
-    if (index === 0) return { [name]: residents.length };
-    return { ...acc, [name]: residents.length };
-  }, '');
+  species.reduce((acc, { name, residents }, index) => (
+    (index === 0) ? ({ [name]: residents.length }) : ({ ...acc, [name]: residents.length })
+  ), '');
 
-function countAnimals(animal = 'all') {
-  if (animal === 'all') return allAnimals();
+function countAnimals(animal) {
+  if (!animal) return allAnimals();
+
   const { specie, sex = 'both' } = animal;
-  const specieInfo = species.find((anim) => anim.name === specie);
-  const specieResidents = specieInfo.residents;
-  const amount = specieResidents.reduce((acc, resident) => {
+  const { residents } = species.find((anim) => anim.name === specie);
+  const amount = residents.reduce((acc, resident) => {
     if (sex === 'both') return acc + 1;
     return (resident.sex === sex) ? acc + 1 : acc;
   }, 0);
