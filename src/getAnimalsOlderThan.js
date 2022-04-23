a664c25e1d41fd547df706fdef1dd533c450ceb0
Guilherme <guilhaugusto05@gmail.com>
2022-04-23 16:06:25 -0300

    refractor getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,8 @@
 const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const animalInfo = species.find((specie) => specie.name === animal).residents;
-  return animalInfo.every((resident) => resident.age >= age);
+  const { residents } = species.find((specie) => specie.name === animal);
+  return residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
